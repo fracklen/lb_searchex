540be4f5654f5d32eda775f34741b899a4f26698
ringling <thomas.ringling@gmail.com>
2015-01-13 12:41:39 +0100

    BoundingBox search function

--- lib/service/location_service.ex
+++ lib/service/location_service.ex
@@ -7,4 +7,9 @@ defmodule LocationService do
     Postalex.Server.locations(ctry_cat, kinds: kinds, postal_districts: postal_districts)
   end
 
+  def by_bounding_box(country, category, kinds, bounding_box) do
+    ctry_cat = %{ country: atomize(country), category: atomize(category) }
+    Postalex.Server.locations(ctry_cat, kinds: kinds, bounding_box: bounding_box)
+  end
+
 end
