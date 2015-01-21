9043f239050d2c57d2729f19f9a10869399fb428
ringling <thomas.ringling@gmail.com>
2015-01-21 16:55:38 +0100

    v0.1.1

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule LbSearchex.Mixfile do
       {:poison, "~> 1.2", [optional: false, hex: :poison, override: true]},
       {:cowboy, "~> 1.0"},
       {:exrm, "~> 0.14.16"},
-      {:postalex, github: "ringling/postalex", tag: "v0.1.0"}
+      {:postalex, github: "ringling/postalex", tag: "v0.1.1"}
     ]
   end
 end
