6be8642503e56e5513e90165d6228d06870aef49
Mads Ohm Larsen <mads.ohm@gmail.com>
2015-03-24 16:11:14 +0100

    Add ConCache; update Postalex

--- lib/lb_searchex.ex
+++ lib/lb_searchex.ex
@@ -1,12 +1,17 @@
 defmodule LbSearchex do
   use Application
 
+  @lb_searchex_cache_name :lb_searchex_cache
+
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     LbSearchex.Endpoint.start
 
-    children = []
+    children = [
+      worker(ConCache, [cache_opts, [name: @lb_searchex_cache_name]])
+    ]
+
     opts = [strategy: :one_for_one, name: LbSearchex.Supervisor]
     Supervisor.start_link(children, opts)
   end
@@ -14,5 +19,12 @@ defmodule LbSearchex do
   def config_change(changed, _new, removed) do
     LbSearchex.Endpoint.config_change(changed, removed)
     :ok
+  end
+
+  defp cache_opts do
+    [
+      ttl: :timer.hours(1),
+      ttl_check: :timer.minutes(5)
+    ]
   end
 end
