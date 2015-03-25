9040ad2b1b03e4494a2fe4caec3f29eef7b12c6d
Anders Emil Nielsen <aemilnielsen@gmail.com>
2015-03-25 09:07:43 +0100

    Configure postalex using mix config

--- config/dev.exs
+++ config/dev.exs
@@ -4,5 +4,13 @@ config :lb_searchex, LbSearchex.Endpoint,
   http: [port: System.get_env("PORT") || 4000],
   debug_errors: true
 
+config :postalex,
+  elastic_server_url: System.get_env("ELASTIC_SERVER_URL") ||
+    "http://localhost:9200",
+  couch_server_url: System.get_env("COUCH_SERVER_URL") ||
+    "http://127.0.0.1:5984",
+  couch_user: System.get_env("COUCH_USER"),
+  couch_pass: System.get_env("COUCH_PASS")
+
 # Enables code reloading for development
 config :phoenix, :code_reloader, true
