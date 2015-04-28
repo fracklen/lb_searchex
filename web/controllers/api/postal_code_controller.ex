202b8fdfd0a09502783672fd142c4dd217814488
Martin Neiiendam <fracklen@gmail.com>
2015-04-28 09:18:06 +0200

    Revert "Add endpoint to look up postal names for postal codes"
    This reverts commit 36ba8ddabb08ad04132333e63317e973adfcfdc4.

--- web/controllers/api/postal_code_controller.ex
+++ web/controllers/api/postal_code_controller.ex
@@ -3,14 +3,6 @@ defmodule LbSearchex.PostalCodeController do
 
   plug :action
 
-  def show(conn, params) do
-    requested_postal_code = params["postal_code"]
-    atomized_country = params["country"] |> String.to_atom
-    postal_code_data = Postalex.Service.PostalCode.fetch_postal_district(%{country: atomized_country, category: nil}, requested_postal_code)
-    allowed_cors = allow_cors(conn)
-    json(allowed_cors, %{postal_code: postal_code_data[:number], postal_name: postal_code_data[:name]})
-  end
-
   def postal_district(conn, params) do
     %{country: params["country"] |> String.to_atom, category: nil}
     |> Postalex.Service.PostalCode.fetch_postal_district(params["postal_code"])
